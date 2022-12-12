bb0a6694972937c6710c561854c91095c10b40ee
JhonM <jhonmajoor@gmail.com>
2022-12-12 23:59:51 +0100

    Adds config workflow

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config.InitialOptions = {
   verbose: true,
   preset: "ts-jest",
   setupFiles: ["dotenv/config"],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.spec.ts",
+    "<rootDir>/frontend/**/__tests__/**/*.spec.ts",
+  ],
 };
 
 export default config;
